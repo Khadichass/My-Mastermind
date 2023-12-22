0ee8a2214dc4546771276de07e726295ebe2e7b8
Khadicha <246abdurashidova123@gmail.com>
2023-12-23 04:34:43 +0500

    some changes added

--- my_mastermind.c
+++ my_mastermind.c
@@ -1,157 +1,265 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-#define CODELEN 4
-typedef unsigned short flag;
 
-//the matched array keeps track of which pieces have been counted so that a piece doesn't get counted as both well placed and misplaced
-static flag matched[CODELEN];
+typedef struct 
+{
+  int wellp;
+  int missp;
+  char* input;
+} info_input;
 
-void mystrcpy(char *, char *);
-void readinput(char *);
-int count_wellplaced(char *, char *);
-int count_misplaced(char *, char *);
+int miss_placed(char* p1, char* p2)
+{
+  int miss = 0;
 
-void mystrcpy(char *s1, char *s2){
-    while((*s1++ = *s2++))
-        ;
+  for (int i = 0; p1[i]; i++) 
+  {
+    for (int j = 0; p2[j]; j++) 
+    {
+      if ((p1[i] == p2[j]) && (i != j)) 
+      {
+        miss++;
+      }
+    }
+  }
+
+  return miss;
 }
 
-void readinput(char *buf){
-    flag invalid;
-    int i;
-    char c;
+int well_placed(char* p1, char* p2)
+{
+  int well = 0;
 
-    do {
-        invalid = 0;
-        i = 0;
+  for (int i = 0; p1[i]; i++) 
+  {
+    if (p1[i] == p2[i]) {
+      well++;
+    }
+  }
 
-        //exits loop on EOF(ctrl+d)(read returns 0) or \n(2nd condition)
-        while(read(0, &c, 1) > 0 && c != '\n')
-            if (c >= '0' && c <= '8' && i < CODELEN)
-                *(buf + i++) = c;
-            else
-                invalid = 1;
+  return well;
+}
 
-        *(buf + CODELEN) = '\0';
+int my_strcmp(char* p1, char*p2)
+{
+  for (int i = 0; p1[i] != '\0'; i++ ) 
+  {
+    if (p1[i] != p2[i]) 
+    {
+      return 1;
+    }
+  }
+  
+  return 0;
+}
 
-        if (c != '\n')
-            printf("\n");
-        
-        if (!invalid && (i == CODELEN))
-            break;
-        
-        // this is needed to pass gandalf
-        if (i == 0) {
-            for(i = 0; i < CODELEN; i++)
-                *(buf + i) = -1;
-            break;
+int my_strcpy(char* p1, char*p2)
+{
+  for (int i = 0; p2[i] != '\0'; i++) 
+  {
+    p1[i] = p2[i];
+  }
+
+  return 0;
+}
+
+int my_atoi(char* p1)
+{
+  int numb = 0;
+  for(int i = 0; p1[i] != '\0'; i++ )
+  {
+    numb += p1[i] - 48;
+    numb *=10;
+
+  }
+
+  return numb / 10;
+}
+
+int my_strchar(char* p1, char p2)
+{
+  for (int i = 0; p1[i] != '\0'; i++) 
+  {
+    if (p1[i] == p2) 
+    {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+char* random_code()
+{
+  srand(time(0));
+
+  char* random_numb = calloc(sizeof(char), 5);
+  int tempr;
+
+  int i = 0;
+
+  while (i < 4) 
+  {
+  
+    tempr = rand() % 9 + 48;
+
+    if (my_strchar(random_numb, tempr) == 0) 
+    {
+      random_numb[i] = tempr;
+      i++;
+    }
+  }
+
+  return random_numb;
+}
+
+char* my_scanf()
+{
+  char* user_input = calloc(sizeof(char), 10);
+  int i = 0;
+  char letter;
+  int ch;
+  while ((ch = read(0, &letter, 1))) 
+  {
+      if (letter == '\n') 
+      {
+          return user_input;
+      }
+      else 
+      {
+          user_input[i] = letter;
+      }
+
+      i++;
+  }
+  if(ch == 0)
+  {
+      printf("exited with CTRL+D\n");
+      exit(0);
+  }
+
+  return user_input;
+}
+
+int my_strlen(char* p1)
+{
+    int length = 0;
+
+    while (p1[length] != '\0') 
+    {
+        length++;
+    }
+
+    return length;
+}
+
+int is_number(char* p1)
+{
+    for (int i = 0; p1[i] != '\0'; i++) 
+    {
+        if (p1[i] < '0' || p1[i] > '8')
+        {
+            return 1;
         }
-        
-        printf("Wrong input!\n");
+    }
 
-        } while (1);
+    return 0;
 }
 
-int count_wellplaced(char *guess, char *codeval){
-    int wellplaced, i;
-    for(wellplaced = 0, i = 0; i < CODELEN; i++, guess++, codeval++)
-        if(*guess == *codeval){
-            matched[i] = 1;
-            *guess = -1;
-            wellplaced++;
-        }
-    return wellplaced;
-}
-
-int count_misplaced(char *guess, char *codeval){
-    int misplaced, i;
-    for(misplaced = 0; *guess != '\0'; guess++)
-        for(i = 0; i < CODELEN; i++)
-            if(!matched[i] && *(codeval + i) == *guess){
-                matched[i] = 1;
-                misplaced++;
-                break;
+int my_repeat (char* p1)
+{
+    
+    for (int i = 0; p1[i]; i++ ) 
+    {
+        for (int j = i + 1; p1[j]; j++ ) 
+        {
+            if (p1[i] == p1[j]) {
+                return 1;
             }
-    return misplaced;
+        }
+    }
+    
+    return 0;
 }
-
-int main(int argc, char **argv){
-
-    // handles incorrect args
-    if(!(argc % 2)){
-        write( 2, "Usage: -t [TRIES] -c [CODE]\n", 31);
+int wrong_input(char* user_input)
+{   
+    if ((my_strlen(user_input) != 4) || (is_number(user_input) || (my_repeat(user_input)))) 
+    {
+        printf("Wrong input!\n");
         return 1;
     }
 
-    int tries = 10;
-    int i;
-    char codeval[CODELEN + 1] = {};
-
-    //checks & assigns program arguments
-    while(--argc){
-        if(**++argv == '-'){
-            argc--;
-            switch(*++*argv){
-                case 'c':
-                    mystrcpy(codeval, *++argv);
-                    for(i = 0; codeval[i] && i < CODELEN; i++)
-                        if(!(codeval[i] >= '0' && codeval[i] <= '8')){
-                            write(2, "Error: invalid piece value\n", 27);
-                            return 1;
-                        }
-                    if (i < CODELEN || codeval[i]){
-                        write(2, "Error: invalid code length\n", 27);
-                        return 1;
-                    }
-                    break;
-                case 't':
-                    for(i = 0, ++argv; *(*argv + i); i++)
-                        if(!(*(*argv + i) >= '0' && *(*argv + i) <= '9')){
-                            write(2, "Error: invalid tries value\n", 27);
-                            return 1;
-                        }
-                    if((tries = atoi(*argv)) == 0){
-                        write(2, "Error: invalid tries value\n", 27);
-                        return 1;
-                    }
-                    break;
-                }
-            }
-    }
-
-    char guess[CODELEN + 1];
-    int misplaced, wellplaced, j;
-
-    //generates a random code
-    if (!codeval[0]){
-        srand((unsigned) time(NULL));
-        for(i = 0; i < CODELEN; i++)
-            codeval[i] = '0' + (rand() % 8);
-    }
-
-    i = 0;
-    printf("Will you find the secret code?\nPlease enter a valid guess\n---\n");
-    do {
-        printf("---\nRound %d\n", i);
-        for (j = 0; j < CODELEN; j++)
-            matched[j] = 0;
-
-        readinput(guess);        
-        
-        wellplaced = count_wellplaced(guess, codeval);
-        misplaced = count_misplaced(guess, codeval);
-        
-        if(wellplaced == CODELEN)
-            break;
-        printf("Well placed pieces: %d\nMisplaced pieces: %d\n", wellplaced, misplaced);
-        if(++i >= tries)
-            break;
-    } while(1);
-    if (wellplaced == CODELEN)
-        printf("Congratz! You did it!\n");
-    else
-        printf("You lost! The code was %s\n", codeval);
     return 0;
+}
+
+void main_work(char* secret_code, int attemp)
+{
+    char* user_input;
+    printf("Will you find the secret code?\nPlease enter a valid guess\n");
+
+    for (int i = 0; i < attemp; i++) 
+    {
+        printf("---\n");
+        printf("Round %d\n", i);
+        write(1, ">", 1);
+        user_input = my_scanf();
+        
+        while (1) 
+        {
+            if (wrong_input(user_input) == 0) 
+            {
+                break;
+            }
+            free(user_input);
+            printf(">");
+            fflush(stdout);
+            user_input = my_scanf();
+        }
+        if(strcmp(secret_code, user_input) == 0)
+        {
+            printf("Congratz! You did it!\n");
+            free(user_input);
+            return;
+        }
+        printf("Well placed pieces: %d\n",well_placed(secret_code,user_input ));
+        printf("Misplaced pieces: %d\n",miss_placed(secret_code,user_input ));
+        free(user_input);
+    }
+
+}
+
+
+int main(int argc, char** argv)
+{
+    char* secret_code = calloc(sizeof(char), 5);
+    int attemp = 10;
+    int flag = 0;
+    if(argc){};
+
+    
+    for(int i = 1; i < argc; i++)
+    {
+        if (my_strcmp(argv[i], "-c") == 0) 
+        {
+            i++;
+            flag = 1;
+            my_strcpy(secret_code, argv[i]);
+        }
+        else if(my_strcmp(argv[i], "-t") == 0)
+        {
+            i++;
+            attemp = my_atoi(argv[i]);
+        }
+       
+    }
+    if(flag == 0)
+    {
+        secret_code = random_code();
+    }
+
+    main_work(secret_code, attemp);
+    free(secret_code);
 }
